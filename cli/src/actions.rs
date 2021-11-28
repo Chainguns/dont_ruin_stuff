0f9aa999875573c3823171416d8b7e1bd87c03fc
RoyB99 <roybarnea10@gmail.com>
2021-11-28 15:13:47 +0200

    Update actions.rs

--- cli/src/actions.rs
+++ cli/src/actions.rs
@@ -23,12 +23,15 @@ struct Log {
     session:Vec<ClientReqRes>,
 }
 
+fn print_err(err: &str) {
+    println!("Error: {}", err.red());
+}
 
-fn read_file(mut file_name:&str) -> Option<String> {
+fn read_file(mut file_name: &str) -> Option<String> {
     let mut file = match File::open(&mut file_name) {
         Ok(f) => f,
         Err(_) => {
-            println!("{}", format!("File \"{}\" not found", file_name).red());
+            print_err(&format!("File \"{}\" not found", file_name));
             return None;
         },
     };
@@ -36,7 +39,7 @@ fn read_file(mut file_name:&str) -> Option<String> {
     match file.read_to_string(&mut file_data) {
         Ok(_) => (),
         Err(_) => {
-            println!("{}", format!("Could not read data from file \"{}\"", file_name));
+            print_err(&format!("Could not read data from file \"{}\"", file_name));
             return None;
         },
     };
@@ -54,9 +57,9 @@ fn method_from_str(m: &str) -> Method{
     }
 }
 
-fn parse_http(file_data:String) -> Vec<Session> {
+fn parse_http(file_data: &str) -> Vec<Session> {
     let mut ret = vec![];
-    match serde_json::from_str::<Vec<Log>>(&file_data) {
+    match serde_json::from_str::<Vec<Log>>(file_data) {
         Ok(logs) => {
             for (i,s) in logs.iter().enumerate() {
                 let mut session = vec![];
@@ -73,17 +76,35 @@ fn parse_http(file_data:String) -> Vec<Session> {
                                 }
                             }    
                         },
-                        Err(e) => {
-                            println!("{}", e);
+                        Err(_) => {
+                            print_err("Failed at parsing the request");
                             continue;
                         }
                     };
                     let mut req_headers = HashMap::new();
                     for h in req1.headers {
-                        req_headers.insert(h.name.to_string(), std::str::from_utf8(h.value).unwrap().to_string());
+                        req_headers.insert(h.name.to_string(), match std::str::from_utf8(h.value) {
+                            Ok(r) => r.to_string(),
+                            Err(_) => {
+                                print_err("Failed at parsing request headers in the logs files");
+                                return vec![];
+                            }
+                        });
                     };
-                    let path = req1.path.unwrap().to_string();
-                    let method = method_from_str(req1.method.unwrap());
+                    let path = match req1.path {
+                        Some(r) => r.to_string(),
+                        None => {
+                            print_err("Failed at getting path from request in the logs files");
+                            return vec![];
+                        }
+                    };
+                    let method = match req1.method {
+                        Some(r) => method_from_str(r),
+                        None => {
+                            print_err("Failed at getting method from request in the logs files");
+                            return vec![];
+                        }
+                    };
                     let req_query = match path.chars().position(|lf| lf == '?') {
                         Some(pos) => {
                             path[pos..].to_string()
@@ -95,7 +116,6 @@ fn parse_http(file_data:String) -> Vec<Session> {
 
                     let mut headers2 = [httparse::EMPTY_HEADER; 24];
                     let mut res1 = httparse::Response::new(&mut headers2);
-                    //let res2 = res1.parse(log.response.as_bytes()).unwrap();
                     let res_payload = match res1.parse(log.response.as_bytes()) {
                         Ok(status) => {
                             match status{
@@ -106,17 +126,28 @@ fn parse_http(file_data:String) -> Vec<Session> {
                                 }
                             }    
                         },
-                        Err(e) => {
-                            println!("{}", e);
+                        Err(_) => {
+                            print_err("Failed at parsing the response in the logs files");
                             continue;
                         }
                     };
                     let mut res_headers = HashMap::new();
                     for h in res1.headers {
-                        res_headers.insert(h.name.to_string(), std::str::from_utf8(h.value).unwrap().to_string());
+                        res_headers.insert(h.name.to_string(), match std::str::from_utf8(h.value) {
+                            Ok(r) => r.to_string(),
+                            Err(_) => {
+                                print_err("Failed at parsing response headers in the logs files");
+                                return vec![];
+                            }
+                        });
                     };
-                    let status = res1.code.unwrap();
-
+                    let status = match res1.code {
+                        Some(r) => r,
+                        None => {
+                            print_err("Failed at getting status from response in the logs files");
+                            return vec![];
+                        }
+                    };
                     session.push(
                         ReqRes{
                             req_headers,
@@ -138,14 +169,15 @@ fn parse_http(file_data:String) -> Vec<Session> {
                 );
             }
         },
-        Err(e) => {
-            println!("{}", e);
+        Err(_) => {
+            print_err("Failed at prasing logs file into Vec<Logs>");
+            return vec![];
         }
     }
     ret
 }
 
-fn _get_sessions(logs:&str) -> Vec<Session> {
+fn vec_sessions_parse(logs: &str) -> Vec<Session> {
     match serde_json::from_str::<Vec<Session>>(logs) {
         Ok(r) => r,
         Err(e0) => {
@@ -154,9 +186,9 @@ fn _get_sessions(logs:&str) -> Vec<Session> {
                     vec![r]
                 },
                 Err(e1) => {
-                    println!("{}", e0);
-                    println!("{}", e1);
-                    println!("{} {}", "ran into an error while parsing your logs".red(), "you can check out the correct format in this address: https://www.blstsecurity.com/firecracker/Documentation".purple());
+                    print_err(&format!("{}", e0));
+                    print_err(&format!("{}", e1));
+                    println!("{} {}", "Ran into an error while parsing your logs".red(), "you can check out the correct format in this address: https://www.blstsecurity.com/firecracker/Documentation".purple());
                     vec![]
                 },
             }
@@ -164,21 +196,54 @@ fn _get_sessions(logs:&str) -> Vec<Session> {
     }
 }
 
+fn get_sessions(logs: &str) -> Vec<Session> {
+    match (parse_http(logs.clone()), vec_sessions_parse(&logs)) {
+        (vec1, vec2) => {
+            if !vec1.is_empty() {
+                vec1
+            } else if !vec2.is_empty() {
+                vec2
+            } else {
+                print_err("Failed parsing the logs");
+                return vec![];
+            }
+        }
+    }
+}
+
 fn write_map_file(file_name:String, map:String) {
-    let mut map_file = OpenOptions::new().write(true).create(true).open(format!("{}.json", &file_name)).unwrap();
-    map_file.write_all(map.as_bytes()).unwrap();
+    match OpenOptions::new().write(true).create(true).open(format!("{}.json", &file_name)) {
+        Ok(mut r) => match r.write_all(map.as_bytes()) {
+            Ok(_) => (),
+            Err(_) => {
+                print_err(&format!("Failed writing to file \"{}\"", &file_name));
+            }
+        },
+        Err(_) => {
+            print_err(&format!("Failed creating \"{}\" file", &file_name));
+        }
+    };
 }
 
 pub fn add_token(token:String) -> bool {
     match Uuid::parse_str(&token) {
         Ok(_) => {
-            let mut map_file = OpenOptions::new().write(true).create(true).open(format!("{}.txt", "token")).unwrap();
-            map_file.write_all(token.as_bytes()).unwrap();
-            println!("{}", "Token added successfuly".green());
-            true
+            match OpenOptions::new().write(true).create(true).open(format!("{}.txt", "token")) {
+                Ok(mut r) => match r.write_all(token.as_bytes()) {
+                    Ok(_) => true,
+                    Err(_) => {
+                        print_err("Failed writing token to file");
+                        false
+                    }
+                },
+                Err(_) => {
+                    print_err("Failed to create token file");
+                    false
+                }
+            }
         },
         Err(e) => {
-            println!("{:?}", e);
+            print_err(&format!("Invalid token, {:?}", e));
             false
         }
     }
@@ -191,18 +256,25 @@ pub fn map(logs_file:String, output:String) {
     let logs = match read_file(&logs_file) {
         Some(r) => r,
         None => {
+            print_err(&format!("Failed reading logs file \"{}\"", &logs_file));
             return;
         }
     };
     let mut digest = Digest::default();
-    let sessions = parse_http(logs.clone());
+    let sessions = get_sessions(&logs);
     if !sessions.is_empty() {
         println!("{}", "Starts mapping...".green());
         digest.load_vec_session(sessions);
-        write_map_file(format!("{}", output), serde_json::to_string(&digest).unwrap());
+        write_map_file(format!("{}", output), match serde_json::to_string(&digest) {
+            Ok(r) => r,
+            Err(_) => {
+                print_err(&format!("Failed writing to \"{}\" file", output));
+                return;
+            }
+        });
         println!("{}", "Your map is ready, you can upload and view it at https://www.blstsecurity.com/firecracker/Visualizer".green());
     } else {
-        println!("{}", "Something went wrong check the errors above".red());
+        print_err("Something went wrong while mapping, check the errors above");
     }
 }
 
@@ -210,16 +282,18 @@ pub fn prepare_attacker(mut url:String, map_file:String) {
     if !executor::block_on(get_access("Prepare")) {
         return;
     }
-    let s_map = match read_file(&format!("{}.json", map_file)){
-        Some(r) => r,
+    let d_map:Digest = match read_file(&format!("{}.json", map_file)){
+        Some(s_map) => {
+            match serde_json::from_str(&s_map) {
+                Ok(r) => r,
+                Err(e) => {
+                    print_err(&format!("Failed getting parsing map to digest, {:?}", e));
+                    return;
+                }
+            }
+        },
         None => {
-            return;
-        }
-    };
-    let d_map:Digest = match serde_json::from_str(&s_map) {
-        Ok(r) => r,
-        Err(e) => {
-            println!("{}", format!("{:?}", e).red());
+            print_err(&format!("Failed reading map file \"{}\"", &map_file));
             return;
         }
     };
@@ -230,7 +304,7 @@ pub fn prepare_attacker(mut url:String, map_file:String) {
             }
         },
         Err(_) => {
-            println!("Invalid url \"{}\"", url.red());
+            print_err(&format!("Invalid url \"{}\"", url));
             return;
         },
     }
@@ -244,16 +318,18 @@ pub async fn attack_domain(map_file:String, decide_file:String, pop:usize, gen:u
     if !executor::block_on(get_access("Attack")) {
         return;
     }
-    let s_map = match read_file(&format!("{}.json", map_file)){
-        Some(r) => r,
+    let d_map:Digest = match read_file(&format!("{}.json", map_file)){
+        Some(s_map) => {
+            match serde_json::from_str(&s_map) {
+                Ok(r) => r,
+                Err(e) => {
+                    print_err(&format!("Failed getting parsing map to digest, {:?}", e));
+                    return;
+                }
+            }
+        },
         None => {
-            return;
-        }
-    };
-    let d_map:Digest = match serde_json::from_str(&s_map) {
-        Ok(r) => r,
-        Err(e) => {
-            println!("{:?}", e);
+            print_err(&format!("Failed reading map file \"{}\"", &map_file));
             return;
         }
     };
@@ -313,23 +389,26 @@ pub fn decide_sessions(logs_file:String, map_file:String) {
     let vec_sessions = match read_file(&format!("{}", logs_file)) {
         Some(r) => r,
         None => {
+            print_err(&format!("Failed reading logs file \"{}\"", &logs_file));
             return;
         }
     }; 
-    let s_map = match read_file(&format!("{}.json", map_file)){
-        Some(r) => r,
+    let d_map:Digest = match read_file(&format!("{}.json", map_file)){
+        Some(s_map) => {
+            match serde_json::from_str(&s_map) {
+                Ok(r) => r,
+                Err(e) => {
+                    print_err(&format!("Failed getting parsing map to digest, {:?}", e));
+                    return;
+                }
+            }
+        },
         None => {
+            print_err(&format!("Failed reading map file \"{}\"", &map_file));
             return;
         }
     };
-    let d_map:Digest = match serde_json::from_str(&s_map) {
-        Ok(r) => r,
-        Err(e) => {
-            println!("{:?}", e);
-            return;
-        }
-    };
-    let anomalys = decide(d_map.clone(), parse_http(vec_sessions)/*get_sessions(&vec_sessions)*/, None);
+    let anomalys = decide(d_map.clone(), get_sessions(&vec_sessions), None);
     let mut a1 = vec![];
     let mut a2 = vec![];
     println!("{}", "Decider starting\nSearching for anomalys...".bold());
@@ -372,29 +451,38 @@ pub fn load(logs_file:String, map_file:String) {
     let logs = match read_file(&logs_file) {
         Some(r) => r,
         None => {
+            print_err(&format!("Failed reading logs file \"{}\"", &logs_file));
             return;
         }
     };
-    let _map = match read_file(&format!("{}.json", map_file)) {
-        Some(r) => r,
+    let mut d_map:Digest = match read_file(&format!("{}.json", map_file)){
+        Some(s_map) => {
+            match serde_json::from_str(&s_map) {
+                Ok(r) => r,
+                Err(e) => {
+                    print_err(&format!("Failed getting parsing map to digest, {:?}", e));
+                    return;
+                }
+            }
+        },
         None => {
+            print_err(&format!("Failed reading map file \"{}\"", &map_file));
             return;
         }
     };
-    let mut digest:Digest = match serde_json::from_str(&map_file) {
-        Ok(r) => r,
-        Err(e) => {
-            println!("{:?}", e);
-            return;
-        }
-    };
-    let sessions = parse_http(logs);//get_sessions(&logs);
+    let sessions =get_sessions(&logs);
     if !sessions.is_empty() {
         println!("{}", "Starts mapping...".green());
-        digest.load_vec_session(sessions);
-        write_map_file(format!("{}", map_file), serde_json::to_string(&digest).unwrap());
+        d_map.load_vec_session(sessions);
+        write_map_file(format!("{}", map_file), match serde_json::to_string(&d_map) {
+            Ok(r) => r,
+            Err(_) => {
+                print_err(&format!("Failed writing to \"{}\" file", map_file));
+                return;
+            }
+        });
         println!("{}", "Your map is ready, you can upload and view it at https://www.blstsecurity.com/firecracker/Visualizer".green());
     } else {
-        println!("{}", "Something went wrong check the errors above".red());
+        print_err("Something went wrong while mapping, check the errors above");
     }
 }
