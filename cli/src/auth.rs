973564b35e358dadb76f4f71ff550427c56740bd
GuyL99 <guy123levinger@gmail.com>
2021-12-21 17:59:41 +0200

    fuck

--- cli/src/auth.rs
+++ cli/src/auth.rs
@@ -1,37 +1,49 @@
-use hyper::{Method, Client, Request , Body, body};
+use colored::*;
+use hyper::{body, Body, Client, Method, Request};
 use hyper_rustls::HttpsConnectorBuilder;
 use std::fs::File;
 use std::io::Read;
-use colored::*;
 
 pub async fn get_access(action: &str) -> bool {
     let mut file = match File::open("token.txt") {
-        Ok(f)=>{
-            f
-        },
-        Err(_)=>{
-            println!("{}", "file \"token.txt\" not found, make sure that you have it in this directory".red());
+        Ok(f) => f,
+        Err(_) => {
+            println!(
+                "{}",
+                "file \"token.txt\" not found, make sure that you have it in this directory".red()
+            );
             println!("{}", "to get your token go to your user details dashboard at https://www.blstsecurity.com/firecracker/UserDetails".purple().bold());
             return false;
-        },
+        }
     };
     let mut token = String::new();
     match file.read_to_string(&mut token) {
         Ok(_) => (),
-        Err(_)=>{
-            println!("{}", "could not read the data from \"token.txt\", make sure the data is valid".red());
+        Err(_) => {
+            println!(
+                "{}",
+                "could not read the data from \"token.txt\", make sure the data is valid".red()
+            );
             println!("{}", "to get your token go to your user details dashboard at https://www.blstsecurity.com/firecracker/UserDetails".purple().bold());
             return false;
-        },
+        }
     }
-    
-    let connector = HttpsConnectorBuilder::new().with_native_roots().https_only().enable_http1().enable_http2().build();
+
+    let connector = HttpsConnectorBuilder::new()
+        .with_native_roots()
+        .https_only()
+        .enable_http1()
+        .enable_http2()
+        .build();
     let client = Client::builder().build(connector);
     let req = Request::builder()
-    .method(Method::POST)
-    .uri("https://cli.blstsecurity.com/auth")
-    .body(Body::from(format!("{{\"client_token\":\"{}\",\"action\":\"{}\"}}", token, action)))
-    .unwrap();
+        .method(Method::POST)
+        .uri("https://cli.blstsecurity.com/auth")
+        .body(Body::from(format!(
+            "{{\"client_token\":\"{}\",\"action\":\"{}\"}}",
+            token, action
+        )))
+        .unwrap();
 
     let r = match client.request(req).await {
         Ok(r) => r,
@@ -41,7 +53,7 @@ pub async fn get_access(action: &str) -> bool {
         }
     };
     let txt = body::to_bytes(r.into_body()).await.unwrap();
-    let json:serde_json::Value = match serde_json::from_slice(&txt){
+    let json: serde_json::Value = match serde_json::from_slice(&txt) {
         Ok(j) => j,
         Err(_) => {
             println!("{}", "client_token not valid".red());
@@ -57,9 +69,12 @@ pub async fn get_access(action: &str) -> bool {
                 println!("{}", json["msg"].to_string().red());
                 false
             }
-        },
+        }
         None => {
-            println!("{}", "error while parsing the response from the authenticator".red());
+            println!(
+                "{}",
+                "error while parsing the response from the authenticator".red()
+            );
             false
         }
     }
